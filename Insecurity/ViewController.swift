aaa65333b148b71207232baac7933e761b15b48c
kevin fernando <kevinfernando799@gmail.com>
2019-05-19 01:26:00 +0700

    Project Done

--- Insecurity/ViewController.swift
+++ Insecurity/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
@@ -14,26 +15,84 @@ class ViewController: UIViewController {
     @IBOutlet weak var balonBiru: UIButton!
     @IBOutlet weak var balonMerah: UIButton!
     @IBOutlet weak var balonAbuAbu: UIButton!
+    @IBOutlet weak var headPhone: UIImageView!
+    @IBOutlet weak var background2: UIImageView!
+    @IBOutlet weak var quote: UILabel!
+    
+    
+    var player: AVAudioPlayer?
+    var player2: AVAudioPlayer?
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        headPhone.alpha = 0
+        quote.alpha = 0
+        quote.textColor = UIColor.white
+        
+        guard let url = Bundle.main.url(forResource: "talkingpeople", withExtension: "wav") else {return}
+        do{
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            if let player = player {
+                
+                player.numberOfLoops = -1
+                player.play()
+            }
+            
+        }
+        
+        catch let error {
+            print(error.localizedDescription)
+        }
+        
+        guard let url2 = Bundle.main.url(forResource: "Flickering", withExtension: "mp3") else {return}
+        do{
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player2 = try AVAudioPlayer(contentsOf: url2, fileTypeHint: AVFileType.mp3.rawValue)
+            if let player = player2 {
+                
+                player.numberOfLoops = -1
+            }
+            
+        }
+            
+        catch let error {
+            print(error.localizedDescription)
+        }
+        
         // Do any additional setup after loading the view.
     }
 
  
     @IBAction func balonCokelatAction(_ sender: Any) {
+        headPhone.alpha = 1
+        player?.stop()
+        balonCokelat.alpha = 0
     }
     
     
     @IBAction func balonBiruAction(_ sender: Any) {
+        background2.image = UIImage(named: "background2")
+        balonBiru.alpha = 0
     }
     
     
     @IBAction func balonMerahAction(_ sender: Any) {
+        player2?.play()
+        balonMerah.alpha = 0
     }
     
     
     @IBAction func balonAbuAbuAction(_ sender: Any) {
+        quote.alpha = 1
+        balonAbuAbu.alpha = 0
     }
     
 }
